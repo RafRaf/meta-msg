d5933902cfb48a6812d4aa6727535a67df8e682c
Rafael <smartrafraf@gmail.com>
2023-08-30 20:47:25 +0700

    Fix readme file

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
 
+description = \
+    "Meta-msg it's an additional layer for your binary exchange " \
+    "systems. It lets you add status/error codes to your messages and " \
+    "raise/handle errors between microservices in traditional (pythonic) way."
+
 setup(
     name='meta-msg',
     version='0.1',
     packages=find_packages(exclude=('tests/*',)),
     author='RafRaf',
     author_email='smartrafraf@gmail.com',
-    # description='BLABLA',
+    description=description,
+    long_description=description,
     license='MIT',
     keywords=['message', 'nats', 'rabbitmq'],
     test_suite='tests',
